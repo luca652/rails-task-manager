1055316fb2b6a87ac0579decc61e0bc5a89368d5
luca gastaldi <lucagastaldi82@gmail.com>
2023-04-29 14:46:55 +0100

    done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+
+  before_action :set_task, only: [:show, :update, :edit, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -17,12 +19,25 @@ class TasksController < ApplicationController
   end
 
   def edit
+  end
 
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
   end
 
   private
 
   def task_params
     params.require(:task).permit(:title, :details)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
